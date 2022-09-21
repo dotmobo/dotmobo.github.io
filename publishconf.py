6ba6472f69ff942d79c7a2250b3d775e9a5ccdef
mobo <morgan.bohn@pc-scol.fr>
2022-09-21 13:39:56 +0200

    https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://dotmobo.github.io'
+SITEURL = 'https://dotmobo.github.io'
 RELATIVE_URLS = False
 
 FEED_ATOM = 'feeds/all.atom.xml'

7b539f6c7de9e49a0538576fdcbe604ce19b44e6
Morgan <morgan.bohn@pc-scol.fr>
2024-08-05 18:12:16 +0200

    change publish conf

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://dotmobo.github.io'
+SITEURL = 'https://dotmobo.xyz'
 RELATIVE_URLS = False
 
 FEED_ATOM = 'feeds/all.atom.xml'

0a91b2013cf2159a70e2e0bef668232cfa422b7b
Morgan <morgan.bohn@pc-scol.fr>
2024-08-05 21:27:15 +0200

    ads.txt, cname

--- pelicanconf.py
+++ pelicanconf.py
@@ -90,11 +90,15 @@ SITEMAP = {
 }
 
 # static
-STATIC_PATHS = ['images', 'css']
+STATIC_PATHS = ['images', 'css', "extra/ads.txt", "extra/CNAME"]
 
 EXTRA_PATH_METADATA = {
     'extra/custom.css': {'path': 'css/custom.css'},
+    "extra/ads.txt": {"path": "ads.txt"},
+    "extra/CNAME": {"path": "CNAME"},
 }
+
+
 CUSTOM_CSS = 'css/custom.css'
 
 USE_LESS = False

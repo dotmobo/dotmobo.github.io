600dec19cfef2f449efcdeea3472a4453fd89f39
Morgan <morgan.bohn@pc-scol.fr>
2024-08-30 20:13:16 +0200

    remove adsense

--- publishconf.py
+++ publishconf.py
@@ -23,17 +23,3 @@ DELETE_OUTPUT_DIRECTORY = True
 DISQUS_SITENAME = "dotmobo"
 
 GOOGLE_ANALYTICS = "UA-68852219-1"
-
-
-GOOGLE_ADSENSE = {
-    'ca_id': 'ca-pub-1630762128981156',
-    'page_level_ads': False,          # Allow Page Level Ads (mobile)
-    'ads': {
-        'aside': '6859931822',          # Side bar banner (all pages)
-        'main_menu': '',      # Banner before main menu (all pages)
-        'index_top': '',      # Banner after main menu (index only)
-        'index_bottom': '',   # Banner before footer (index only)
-        'article_top': '',    # Banner after article title (article only)
-        'article_bottom': '', # Banner after article content (article only)
-    }
-}
